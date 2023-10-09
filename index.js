99f16a745e960f5596776595fcfd18380dd6819a
caroolt <carolinatc1405@gmail.com>
2023-10-09 16:41:54 -0300

    [Api Pensador] chamada

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
 const express = require('express');
+const cors = require('cors'); // Importe o módulo cors
+
 const app = express();
+
+// Configure o CORS para aceitar solicitações de todas as origens
+app.use(cors());
 
 app.get('/frases/:keywords/:pages', async (req, res) => {
   try {
     const keywords = req.params.keywords; 
     const pages = req.params.pages;
     
-     const frases = [];
+    const frases = [];
     for(i = 1; i <= pages; i++) {
-      let response
+      let response;
       if(i === 1 ) {
          response = await axios.get(`https://www.pensador.com/${keywords}/`);
       } else {
         response = await axios.get(`https://www.pensador.com/${keywords}/${i}/`);
       }
     
-    const pensador = cheerio.load(response.data);
-
-
-    const fraseElements = pensador('.frase');
+      const pensador = cheerio.load(response.data);
+      const fraseElements = pensador('.frase');
   
-    fraseElements.each((index, element) => {
-      const frase = pensador(element).text();
-      frases.push(frase);
-    });
+      fraseElements.each((index, element) => {
+        const frase = pensador(element).text();
+        frases.push(frase);
+      });
     }
-    
 
     res.json({
       frases
